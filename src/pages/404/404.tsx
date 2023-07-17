979f315e50933c8795901d012753c48ab707957a
Greg <grtop@yandex.ru>
2023-07-17 22:45:46 +0300

    правим сообщение

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -3,7 +3,9 @@ import {Link} from 'react-router-dom';
 function NotFound(): JSX.Element {
   return (
     <>
-      <h1>404 Page Not Found</h1>
+      <h1>404,
+        <br /> Page Not Found
+      </h1>
       <Link to='/'>Вернуться на главную</Link>
     </>
   );
