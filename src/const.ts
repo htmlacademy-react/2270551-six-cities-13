cbe3df10794fd8f42b9b80bd26b537147f957120
Greg <grtop@yandex.ru>
2023-07-17 18:55:26 +0300

    добавляем константы

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,21 @@
 /*Константы*/
 
-export const enum Settings {
-  offers = 312
+const enum Settings {
+  OFFERS = 312,
 }
+
+enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Offer = '/offer/:id',
+  Favorites = '/favorites',
+}
+
+enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
+
+export{Settings, AppRoute, AuthorizationStatus};
+
