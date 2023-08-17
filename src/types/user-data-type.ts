ad1926bd7851634b32a8c0d2590b6dc679c4cb55
Greg <grtop@yandex.ru>
2023-08-17 10:56:46 +0300

    авторизация

--- src/types/user-data-type.ts
+++ src/types/user-data-type.ts
@@ -1,16 +1,7 @@
 export type UserData = {
   id: number;
   email: string;
+  avatarUrl: string;
+  isPro: boolean;
   token: string;
 }
-
-/*export type UserData = {
-  avatarUrl: string;
-  email: string;
-  id: number;
-  isPro: boolean;
-  name: string;
-  token: string;
-};
-
-export default UserData;*/
