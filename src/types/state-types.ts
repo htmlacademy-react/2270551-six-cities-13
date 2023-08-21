bef3e77b9c5c689559851567ba3827493dba8a0c
Greg <grtop@yandex.ru>
2023-08-21 20:12:18 +0300

    правка

--- src/types/state-types.ts
+++ src/types/state-types.ts
@@ -1,6 +1,37 @@
 import {store} from '../store';
+import {AuthorizationStatus, RequestStatus} from '../const';
+import {Review} from './review-types';
+import {AuthorizedUser} from './user-data-type';
+import {City, Offer, DetailOffer} from './offer-types';
+
+export type UserData = {
+  user: AuthorizedUser | null;
+  authorizationStatus: AuthorizationStatus;
+  sendingStatusLogin: RequestStatus;
+}
+
+export type OffersData = {
+  offers: Offer[];
+  fetchingStatusOffers: RequestStatus;
+  activeCity: City;
+}
+
+export type OfferData = {
+  offer: DetailOffer | null;
+  fetchingStatusOffer: RequestStatus;
+}
+
+export type ReviewsData = {
+  reviews: Review[];
+  fetchingStatusReviews: RequestStatus;
+  sendingStatusReview: RequestStatus;
+}
+
+export type NearbyData = {
+  nearby: Offer[];
+  fetchingStatusNearby: RequestStatus;
+}
 
 export type State = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
-
