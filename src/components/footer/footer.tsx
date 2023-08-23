c0f6334460596b452213a06af111fe6b28b2fbdc
Greg <grtop@yandex.ru>
2023-08-24 00:07:48 +0300

    включил заранее подготовленное

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,4 +1,4 @@
-/*import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 function Footer(): JSX.Element {
 
@@ -11,4 +11,5 @@ function Footer(): JSX.Element {
   );
 }
 
-export default Footer;*/
+export default Footer;
+
