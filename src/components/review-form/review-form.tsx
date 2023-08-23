a0e21a1a6231cbe3db1f5bdbd22084d050e46f91
Greg <grtop@yandex.ru>
2023-08-23 20:28:02 +0300

    рефакторинг

--- src/components/review-form/review-form.tsx
+++ src/components/review-form/review-form.tsx
@@ -1,8 +1,11 @@
 import {useState, useEffect, FormEvent, ChangeEvent, Fragment} from 'react';
+import {toast} from 'react-toastify';
 import {useAppDispatch, useAppSelector} from '../../hooks';
-import {dropSendingStatus} from '../../store/action';
-import {postReview} from '../../store/api-action';
 import {RequestStatus} from '../../const';
+import {Offer} from '../../types/offer-types';
+import {postReview} from '../../store/api-action';
+import {dropReviewSendingStatus} from '../../store/reviews-data/reviews-data.slice';
+import {getReviewSendingStatus} from '../../store/reviews-data/reviews-data.selectors';
 
 const CommentLength = {MIN: 50, MAX: 300};
 
@@ -15,21 +18,26 @@ const ratingMap = {
 };
 
 type ReviewSendFormProps = {
-  id: string;
+  id: Offer['id'];
 }
 
-function ReviewSendForm({id}: ReviewSendFormProps) {
-  const [comment, setComment] = useState('');
+function ReviewSendForm({id}: ReviewSendFormProps): JSX.Element {
   const [rating, setRating] = useState('');
+  const [comment, setComment] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const dispatch = useAppDispatch();
-  const sendingStatus = useAppSelector((state) => state.sendingReviewStatus);
+  const sendingStatus = useAppSelector(getReviewSendingStatus);
 
   const isValid =
     comment.length >= CommentLength.MIN &&
     comment.length <= CommentLength.MAX &&
     rating !== '';
+
+  const handleFormSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    dispatch(postReview({reviewData: {comment, rating: Number(rating)}, id}));
+  };
 
   function handleRatingChange(evt: ChangeEvent<HTMLInputElement>) {
     setRating(evt.target.value);
@@ -39,33 +47,36 @@ function ReviewSendForm({id}: ReviewSendFormProps) {
     setComment(evt.target.value);
   }
 
-  const handleFormSubmit = (evt: FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
-    dispatch(postReview({reviewData: {comment, rating: +rating}, id}));
-  };
-
   useEffect(() => {
-    switch (sendingStatus) {
-      case RequestStatus.Success:
-        setComment('');
-        setRating('');
-        dispatch(dropSendingStatus());
-        break;
-      case RequestStatus.Pending:
-        setIsSubmitting(true);
-        break;
-      default:
-        setIsSubmitting(false);
+    let isMounted = true;
+
+    if (isMounted) {
+      switch (sendingStatus) {
+        case RequestStatus.Success:
+          setRating('');
+          setComment('');
+          dispatch(dropReviewSendingStatus());
+          break;
+        case RequestStatus.Pending:
+          setIsSubmitting(true);
+          break;
+        case RequestStatus.Error:
+          toast.warn('Комментарий не отправлен');
+          setIsSubmitting(false);
+          break;
+        default:
+          setIsSubmitting(false);
+      }
     }
+
+    return () => {
+      isMounted = false;
+    };
   }, [sendingStatus, dispatch]);
 
   return (
     <form className="reviews__form form" action="#" method="post" onSubmit={handleFormSubmit}>
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
-
-      {sendingStatus === RequestStatus.Error &&
-        <p>Не удалось отправить комментарий, попробуйте еще раз!</p>}
-
       <div className="reviews__rating-form form__rating">
         {Object.entries(ratingMap)
           .reverse()
