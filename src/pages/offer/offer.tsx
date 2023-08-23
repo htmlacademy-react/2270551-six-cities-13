0884bd48c38e6a7e6cbe1f31dbb708a77003273f
Greg <grtop@yandex.ru>
2023-08-24 00:03:46 +0300

    рефакторинг оферов

--- src/pages/offer/offer.tsx
+++ src/pages/offer/offer.tsx
@@ -3,22 +3,24 @@
 import {useState, useEffect} from 'react';
 import {Helmet} from 'react-helmet-async';
 import {useParams} from 'react-router-dom';
-import {useAppSelector, useAppDispatch} from '../../hooks';
+import {useAppDispatch, useAppSelector} from '../../hooks';
 import {AuthorizationStatus, RequestStatus} from '../../const';
-import {fetchOfferAction, fetchReviewsAction, fetchOffersNearbyAction} from '../../store/api-action';
 import {Offer} from '../../types/offer-types';
+import {fetchOfferAction, fetchReviewsAction, fetchOffersNearbyAction, fetchFavoritesAction} from '../../store/api-action';
 import {getAuthorizationStatus} from '../../store/user-data/user-data.selectors';
 import {getOffer, getOfferFetchingStatus} from '../../store/offer-data/offer-data.selectors';
 import {getReviews} from '../../store/reviews-data/reviews-data.selectors';
 import {getOffersNearby} from '../../store/nearby-data/nearby-data.selectors';
+import {OffersListMemo as OffersList} from '../../components/offer-list/offer-list';
 import Map from '../../components/map/map';
 import Loader from '../../pages/loading-page/loading-page';
 import HeaderFull from '../../components/header/header-full';
-import OffersList from '../../components/offer-list/offer-list';
 import ReviewList from '../../components/review-list/review-list';
 import ReviewSendForm from '../../components/review-form/review-form';
 import OfferGallery from '../../components/offer-gallery/offer-gallery';
 import DetailOffer from '../../components/detailer-offer/detailer-offer';
+
+const NEARBY_OFFERS_COUNT = 3;
 
 function OfferPage(): JSX.Element {
   const params = useParams();
@@ -30,13 +32,8 @@ function OfferPage(): JSX.Element {
 
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    if (params.id) {
-      dispatch(fetchOfferAction(params.id));
-      dispatch(fetchReviewsAction(params.id));
-      dispatch(fetchOffersNearbyAction(params.id));
-    }
-  }, [dispatch, params.id]);
+  const offersNearbyOnTheMap = offersNearby.slice(0, NEARBY_OFFERS_COUNT);
+  const threeOffersNearby = offersNearbyOnTheMap.slice(0, NEARBY_OFFERS_COUNT);
 
   const [selectedPoint, setSelectedPoint] = useState<Offer | undefined>(
     undefined
@@ -51,6 +48,19 @@ function OfferPage(): JSX.Element {
 
     setSelectedPoint(currentPoint);
   };
+
+  useEffect(() => {
+    if (params.id) {
+      dispatch(fetchOfferAction(params.id));
+      dispatch(fetchReviewsAction(params.id));
+      dispatch(fetchOffersNearbyAction(params.id));
+      dispatch(fetchFavoritesAction());
+    }
+  }, [dispatch, params.id]);
+
+  if (offer) {
+    offersNearbyOnTheMap.push(offer);
+  }
 
   if (isDetailedOfferDataLoading === RequestStatus.Pending){
     return (
@@ -67,25 +77,25 @@ function OfferPage(): JSX.Element {
       {isDetailedOfferDataLoading === RequestStatus.Success && offer &&
       <main className="page__main page__main--offer">
         <section className="offer">
-          <OfferGallery offer={offer} />
+          <OfferGallery offer={offer}/>
           <div className="offer__container container">
             <div className="offer__wrapper">
-              <DetailOffer offer={offer} />
+              <DetailOffer offer={offer}/>
               <section className="offer__reviews reviews">
                 <ReviewList reviews={reviews}/>
                 {isAuthorizationStatus === AuthorizationStatus.Auth &&
-                <ReviewSendForm id={params.id as string} />}
+                <ReviewSendForm id={params.id as string}/>}
               </section>
             </div>
           </div>
           <section className="offer__map map">
-            <Map city={offer.city} points={offersNearby} selectedPoint={selectedPoint}/>
+            <Map city={offer.city} points={offersNearbyOnTheMap} detailedOffer={offer} selectedPoint={selectedPoint}/>
           </section>
         </section>
         <div className="container">
           <section className="near-places places">
             <h2 className="near-places__title">Other places in the neighbourhood</h2>
-            <OffersList type='near-places' offers={offersNearby} onOfferCardHover={handleOfferCardHover}/>
+            <OffersList type='near-places' offers={threeOffersNearby} onOfferCardHover={handleOfferCardHover}/>
           </section>
         </div>
       </main>}
