6fb80092c585abcb599939ab7ab8b212dcbaa375
Greg <grtop@yandex.ru>
2023-08-24 00:12:54 +0300

    мемоизация и изменение отображения города

--- src/components/cities-list/cities-list.tsx
+++ src/components/cities-list/cities-list.tsx
@@ -1,3 +1,4 @@
+import {memo} from 'react';
 import {Link} from 'react-router-dom';
 import {useAppDispatch} from '../../hooks';
 import {CityMap} from '../../const';
@@ -12,30 +13,37 @@ function CitiesList({activeCity}: CitiesListProps): JSX.Element {
   const dispatch = useAppDispatch();
 
   return (
-    <ul className="locations__list tabs__list">
-      {Object.values(CityMap).map((city, i) => {
-        const keyValue = `${city.name}-${i}`;
-        return (
-          <li className="locations__item" key={keyValue}>
-            <Link
-              className={classNames({
-                'locations__item-link': true,
-                'tabs__item': true,
-                'tabs__item--active': activeCity === city.name
-              })}
-              to="#"
-              onClick={(evt) => {
-                evt.preventDefault();
-                dispatch(setActiveCity(city));
-              }}
-            >
-              <span>{city.name}</span>
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <h1 className="visually-hidden">Cities</h1>
+      <div className="tabs">
+        <section className="locations container">
+          <ul className="locations__list tabs__list">
+            {Object.values(CityMap).map((city, i) => {
+              const keyValue = `${city.name}-${i}`;
+              return (
+                <li className="locations__item" key={keyValue}>
+                  <Link className={classNames({
+                    'locations__item-link': true,
+                    'tabs__item': true,
+                    'tabs__item--active': activeCity === city.name
+                  })}
+                  to="#"
+                  onClick={(evt) => {
+                    evt.preventDefault();
+                    dispatch(setActiveCity(city));
+                  }}
+                  >
+                    <span>{city.name}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+      </div>
+    </>
   );
 }
 
-export default CitiesList;
+export const CitiesListMemo = memo(CitiesList);
+
