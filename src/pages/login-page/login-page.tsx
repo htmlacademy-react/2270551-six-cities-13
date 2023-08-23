5624503c75dc415efc409333caf48950c23c629d
Greg <grtop@yandex.ru>
2023-08-24 00:05:03 +0300

    добавил регулярное выражение проверка пароля

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,28 +1,62 @@
 /*Компонент формы регистрации*/
 
-import {useRef, FormEvent} from 'react';
-import {Link} from 'react-router-dom';
+import {useEffect, useRef, FormEvent} from 'react';
+import {useNavigate} from 'react-router-dom';
 import {Helmet} from 'react-helmet-async';
-import {useAppDispatch} from '../../hooks';
+import {toast} from 'react-toastify';
+import {useAppDispatch, useAppSelector} from '../../hooks';
+import {AppRoute, AuthorizationStatus, CityMap} from '../../const';
+import {getRandomCity} from '../../utils';
 import {loginAction} from '../../store/api-action';
+import {setActiveCity} from '../../store/offers-data/offers-data.slice';
+import {getAuthorizationStatus} from '../../store/user-data/user-data.selectors';
 import HeaderLeft from '../../components/header/header-left';
 
 function LoginPage(): JSX.Element {
   const loginRef = useRef<HTMLInputElement | null>(null);
   const passwordRef = useRef<HTMLInputElement | null>(null);
 
+  const regexEmail = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
+  const regexPassword = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
+
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
+  const isAuthorizationStatus = useAppSelector(getAuthorizationStatus);
+
+  const randomCity = getRandomCity(CityMap);
+
+  const handleButtonRandomClick = () => {
+    dispatch(setActiveCity(randomCity));
+    navigate(AppRoute.Main);
+  };
 
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) =>{
     evt.preventDefault();
 
     if (loginRef.current && passwordRef.current) {
+
+      if(!regexEmail.test(loginRef.current.value)) {
+        toast.warn('Invalid login form');
+        return;
+      }
+
+      if(!regexPassword.test(passwordRef.current.value)) {
+        toast.warn('Invalid password form');
+        return;
+      }
+
       dispatch(loginAction({
         login: loginRef.current.value,
         password: passwordRef.current.value
       }));
     }
   };
+
+  useEffect(() => {
+    if (isAuthorizationStatus === AuthorizationStatus.Auth) {
+      navigate(AppRoute.Main);
+    }
+  }, [isAuthorizationStatus, navigate]);
 
   return (
     <div className="page page--gray page--login">
@@ -55,9 +89,9 @@ function LoginPage(): JSX.Element {
           </section>
           <section className="locations locations--login locations--current">
             <div className="locations__item">
-              <Link className="locations__item-link" to="#">
-                <span>Amsterdam</span>
-              </Link>
+              <button className="locations__item-link" type='button' onClick={handleButtonRandomClick}>
+                <span>{randomCity.name}</span>
+              </button>
             </div>
           </section>
         </div>
