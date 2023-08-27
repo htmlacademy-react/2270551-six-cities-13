eaeaa742c7138435adafa164210341e1f11a421c
Greg <grtop@yandex.ru>
2023-08-27 11:47:59 +0300

    грамматика

--- src/const.ts
+++ src/const.ts
@@ -58,47 +58,47 @@ export const CityMap: Record<CityName, City> = {
     location: {
       latitude: 48.85661,
       longitude: 2.351499,
-      zoom: 13
-    }
+      zoom: 13,
+    },
   },
   [CityName.Cologne]: {
     name: CityName.Cologne,
     location: {
       latitude: 50.938361,
       longitude: 6.959974,
-      zoom: 13
-    }
+      zoom: 13,
+    },
   },
   [CityName.Brussels]: {
     name: CityName.Brussels,
     location: {
       latitude: 50.846557,
       longitude: 4.351697,
-      zoom: 13
-    }
+      zoom: 13,
+    },
   },
   [CityName.Amsterdam]: {
     name: CityName.Amsterdam,
     location: {
       latitude: 52.37454,
       longitude: 4.897976,
-      zoom: 13
-    }
+      zoom: 13,
+    },
   },
   [CityName.Hamburg]: {
     name: CityName.Hamburg,
     location: {
       latitude: 53.550341,
       longitude: 10.000654,
-      zoom: 13
-    }
+      zoom: 13,
+    },
   },
   [CityName.Dusseldorf]: {
     name: CityName.Dusseldorf,
     location: {
       latitude: 51.225402,
       longitude: 6.776314,
-      zoom: 13
-    }
+      zoom: 13,
+    },
   },
 } as const;
