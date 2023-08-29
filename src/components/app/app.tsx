3d5d9117dedf344ca28c3b70cc58a9b1d681f4c5
Greg <grtop@yandex.ru>
2023-08-29 20:52:36 +0300

    устранение первого и второго замечания

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,11 @@
 /*Компонент для отрисовки главной страницы*/
 
+import {useEffect} from 'react';
 import {HelmetProvider} from 'react-helmet-async';
 import {Routes, Route} from 'react-router-dom';
-import {useAppSelector} from '../../hooks';
-import {AppRoute} from '../../const';
+import {useAppSelector, useAppDispatch} from '../../hooks';
+import {checkAuthAction, fetchFavoritesAction} from '../../store/api-action';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import {getAuthorizationStatus} from '../../store/user-data/user-data.selectors';
 import MainPage from '../../pages/main/main';
 import LoginPage from '../../pages/login-page/login-page';
@@ -13,10 +15,28 @@ import NotFoundPage from '../../pages/404/404';
 import PrivateRoute from '../private-route/private-route';
 import HistoryRouter from '../history-router/history-router';
 import browserHistory from '../../browser-history';
+import LoadingPage from '../../pages/loading-page/loading-page';
 
 function App(): JSX.Element {
   //const isAuthorizationStatus = useAppSelector(getAuthorizationStatus);
+  //store.dispatch(checkAuthAction());
+  const dispatch = useAppDispatch();
 
+  const isAuthorizationStatus = useAppSelector(getAuthorizationStatus);
+
+  useEffect(() => {
+    dispatch(checkAuthAction());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isAuthorizationStatus === AuthorizationStatus.Auth) {
+      dispatch(fetchFavoritesAction());
+    }
+  }, [dispatch, isAuthorizationStatus]);
+
+  if (isAuthorizationStatus === AuthorizationStatus.Unknown) {
+    return <LoadingPage />;
+  }
   return (
     <HelmetProvider>
       <HistoryRouter history={browserHistory}>
@@ -38,7 +58,7 @@ function App(): JSX.Element {
           <Route
             path={AppRoute.Favorites}
             element={
-              <PrivateRoute authorizationStatus={useAppSelector(getAuthorizationStatus)}>
+              <PrivateRoute authorizationStatus={isAuthorizationStatus}>
                 <FavoritesPage/>
               </PrivateRoute>
             }
